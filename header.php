1364f843660c5d30cdd5027ae35b9c12d13716eb
Haideep <raihaideep@gmail.com>
2022-08-01 23:18:45 +0545

    first commit

---
+++ header.php
@@ -0,0 +1,128 @@
+<head>
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link rel='stylesheet' type='text/css' href="css/bootstrap.css"/>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="css/header.css"/>
+</head>
+<header>
+<div class="header">
+	<div class="logo">
+		<a href="index.php">Advanced Accounting And Tax Solutions</a>
+	</div>
+</div>
+<?php  
+  if (isset($_GET['error'])) {
+		if ($_GET['error'] == "wrongpasswordup") {
+			echo '	<script type="text/javascript">
+					 	setTimeout(function () {
+			                $(".up_info1").fadeIn(200);
+			                $(".up_info1").text("The password is wrong!!");
+			                $("#admin-account").modal("show");
+		              	}, 500);
+		              	setTimeout(function () {
+		                	$(".up_info1").fadeOut(1000);
+		              	}, 3000);
+					</script>';
+		}
+	} 
+	if (isset($_GET['success'])) {
+		if ($_GET['success'] == "updated") {
+			echo '	<script type="text/javascript">
+			 			setTimeout(function () {
+			                $(".up_info2").fadeIn(200);
+			                $(".up_info2").text("Your Account has been updated");
+              			}, 500);
+              			setTimeout(function () {
+                			$(".up_info2").fadeOut(1000);
+              			}, 3000);
+					</script>';
+		}
+	}
+	if (isset($_GET['login'])) {
+	    if ($_GET['login'] == "success") {
+	      echo '<script type="text/javascript">
+	              
+	              setTimeout(function () {
+	                $(".up_info2").fadeIn(200);
+	                $(".up_info2").text("You successfully logged in");
+	              }, 500);
+
+	              setTimeout(function () {
+	                $(".up_info2").fadeOut(1000);
+	              }, 4000);
+	            </script> ';
+	    }
+	  }
+?>
+<div class="topnav" id="myTopnav">
+
+	<a href="index.php" <?php if($_SERVER['SCRIPT_NAME']=="/rfidattendance/index.php") { ?>  class="active"   <?php   }  ?>>Employees</a>
+    <a href="ManageUsers.php" <?php if($_SERVER['SCRIPT_NAME']=="/rfidattendance/ManageUsers.php") { ?>  class="active"   <?php   }  ?>>Manage Employees</a>
+    <a href="UsersLog.php" <?php if($_SERVER['SCRIPT_NAME']=="/rfidattendance/UsersLog.php") { ?>  class="active"   <?php   }  ?>>Employees Log</a>
+    <!-- <a href="devices.php">Devices</a> -->
+	<style>
+		.active{
+			background-color: #fbffcb;
+			color: black !important;
+		}
+	</style>
+    <?php  
+    	if (isset($_SESSION['Admin-name'])) {
+    		echo '<a href="#" data-toggle="modal" data-target="#admin-account">'.$_SESSION['Admin-name'].'</a>';
+    		echo '<a href="logout.php">Log Out</a>';
+    	}
+    	else{
+    		echo '<a href="login.php">Log In</a>';
+    	}
+    ?>
+    <a href="javascript:void(0);" class="icon" onclick="navFunction()">
+	  <i class="fa fa-bars"></i></a>
+</div>
+<div class="up_info1 alert-danger alerting mx-4 my-4 " ></div>
+<div class="up_info2 alert-success alerting mx-4 my-4" ></div>
+</header>
+<style>
+	
+</style>
+<script>
+	function navFunction() {
+	  var x = document.getElementById("myTopnav");
+	  if (x.className === "topnav") {
+	    x.className += " responsive";
+	  } else {
+	    x.className = "topnav";
+	  }
+	}
+</script>
+
+<!-- Account Update -->
+<div class="modal fade" id="admin-account"  tabindex="-1" role="dialog" aria-labelledby="Admin Update" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered" role="document">
+    <div class="modal-content">
+      <div class="modal-header" style="background: #b3c6ff !important; color:black !important;">
+        <h3 class="modal-title" id="exampleModalLongTitle">Update Your Account Info:</h3>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <form action="ac_update.php" method="POST" enctype="multipart/form-data">
+	      <div class="modal-body" style="background:white !important; color:black !important;">
+	      	<label for="User-mail"><b>Admin Name:</b></label>
+	      	<input type="text" name="up_name" placeholder="Enter your Name..." value="<?php echo $_SESSION['Admin-name']; ?>" required/><br>
+	      	<label for="User-mail"><b>Admin E-mail:</b></label>
+	      	<input type="email" name="up_email" placeholder="Enter your E-mail..." value="<?php echo $_SESSION['Admin-email']; ?>" required/><br>
+	      	<label for="User-psw"><b>Password</b></label>
+	      	<input type="password" name="up_pwd" placeholder="Enter your Password..." required/><br>
+	      </div>
+	      <div class="modal-footer" style="background: #b3c6ff !important; color:black !important;">
+	        <button type="submit" name="update" class="btn btn-success">Save changes</button>
+	        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+	      </div>
+	  </form>
+    </div>
+  </div>
+</div>
+<!-- //Account Update -->
+	
+
+	
